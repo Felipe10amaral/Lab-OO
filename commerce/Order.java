9369e9bfd7d4b60b404d75c24cb3c903ed794e32
Felipe Amaral <felipeamaraldev15@gmail.com>
2022-10-03 23:39:36 -0300

    implementando classe Order

--- commerce/Order.java
+++ commerce/Order.java
@@ -1,5 +1,24 @@
 package commerce;
 
+import java.util.Map;
+import java.util.Set;
+
+import business.Customer;
+import business.Manager;
+import business.Product;
+import business.Seller;
+
 public class Order {
+    private static final int MAX_RESPONSIBLE = 5;
+    private static final int MIN_SELLERS = 1;
+    private Customer customer;
+    private Manager[] responsibles;
+    private int numberOfResponsibles;
+    private Set<Seller> seller;
+    private Map<Product, Item> items;
+
+    public Order(Customer customer, Seller seller) {
+
+    }
 
 }
